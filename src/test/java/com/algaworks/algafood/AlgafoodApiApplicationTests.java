4c7229c1ad135ca34c974391cca523a47d49e03f
Ronaldo Melo <ronaldorpm1994@gmail.com>
2023-03-20 22:56:53 -0300

    10.5. Desafio: escrevendo testes de integracao

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.junit.jupiter.api.Test;
@@ -9,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.validation.ConstraintViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 //@RunWith(SpringRunner.class)
@@ -33,14 +34,23 @@ public class AlgafoodApiApplicationTests {
 
 	@Test
 	public void testarFalharAoCadastrarQuandoSemNome(){
-
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
-		//ação
 		ConstraintViolationException erroEsperado = assertThrows(ConstraintViolationException.class, () -> cadastroCozinhaService.salvar(novaCozinha));
-		//validação
 		assertThat(erroEsperado).isNotNull();
 
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso(){
+		EntidadeEmUsoException erroEsperado = assertThrows(EntidadeEmUsoException.class, () -> cadastroCozinhaService.excluir(1L));
+		assertThat(erroEsperado).isNotNull();
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente(){
+		CozinhaNaoEncontradaException erroEsperado = assertThrows(CozinhaNaoEncontradaException.class, () -> cadastroCozinhaService.excluir(10L));
+		assertThat(erroEsperado).isNotNull();
 	}
 
 }
